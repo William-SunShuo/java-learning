441d8e8341e11267d369d187b043fe6d51db470d
shuo.sun <shuo.sun@chivox.com>
2021-03-29 17:41:11 +0800

    -----

--- src/main/java/Rx/RxTest.java
+++ src/main/java/Rx/RxTest.java
@@ -3,12 +3,7 @@ package Rx;
 public class RxTest {
 
     public static void main(String[] args){
-      Observable.create(new ObservableOnSubscribe<String>() {
-          @Override
-          public void subscribe(Emitter<String> emitter) throws Throwable {
-              emitter.onNext("123");
-          }
-      }).map(s -> Integer.parseInt(s)).subscribe(new Observer<Integer>() {
+      Observable.create((ObservableOnSubscribe<String>) emitter -> emitter.onNext("123")).map(Integer::parseInt).subscribe(new Observer<Integer>() {
           @Override
           public void onSubscribe() {
 
--- src/main/java/designpatten/vistor/VisitorTest.java
+++ src/main/java/designpatten/vistor/VisitorTest.java
@@ -3,6 +3,11 @@ import java.io.File;
 
 public class VisitorTest {
 
+    /**
+     * 访问者模式的核心思想是为了访问复杂的数据结构，而不去改变数据结构，而是把对数据的操作抽象出来
+     * 在访问的过程中以回调形式在访问者中处理操作逻辑。如果要新增一组操作，只需要增加一个新的访问者
+     */
+
     public static void main(String[] args) {
         FileStructure fs = new FileStructure(new File("src/main/java"));
         fs.handle(new JavaFileVisitor());
